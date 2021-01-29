c99a1a8ba28c2213487275a865d41ba4d871ff7b
michamaro <michamaro52@gmail.com>
2021-01-29 20:13:21 +0900

    [update]ログイン画面の構成

--- backend/resources/views/login.php
+++ backend/resources/views/login.php
@@ -1,6 +1,38 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>ログイン</title>
+    </head>
 <body>
-<?php print("testです")?>
+    <div align="center">
+        <table border="0">
+            <form action="list.html" method="get">
+                <tr>
+                    <th>
+                        ID
+                    </th>
+                    <td>
+                        <input type="text" name="user_id" value="" size="24">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        パスワード
+                    </th>
+                    <td>
+                        <input type="password" name="password" value="" size="24">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div align="center">
+                            <input type="submit" value="ログイン">
+                        </div>
+                    </td>
+                </tr>
+            </form>
+        </table>
+    </div>
 </body>
 </html>
