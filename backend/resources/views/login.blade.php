9974fa55ce293d3aea82e675ee0d26df1010f483
michamaro <michamaro52@gmail.com>
2021-01-31 17:52:51 +0900

    [update]ログイン画面のデザイン修正

--- backend/resources/views/login.blade.php
+++ backend/resources/views/login.blade.php
@@ -1,38 +1,35 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/005c1b89ac.js" crossorigin="anonymous"></script>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <title>ログイン</title>
     </head>
-<body>
-    <div align="center">
-        <table border="0">
+    <body>
+        <div class="mx-auto" style="width: 200px;">
             <form action="list.html" method="get">
-                <tr>
-                    <th>
-                        ID
-                    </th>
-                    <td>
-                        <input type="text" name="user_id" value="" size="24">
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        パスワード
-                    </th>
-                    <td>
-                        <input type="password" name="password" value="" size="24">
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <div align="center">
-                            <input type="submit" value="ログイン">
+                <div class="form-group">
+                <i class="fas fa-user"></i>
+                    <label for="inputId"> ID</label>
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <input type="id" class="form-control" id="id" aria-describedby="idHelp" placeholder="ID">
                         </div>
-                    </td>
-                </tr>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <i class="fas fa-key"></i>
+                    <label for="inputPass">パスワード</label>
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <input type="password" class="form-control" id="pass" placeholder="パスワード">
+                        </div>
+                    </div>
+                </div><br/>
+                <button type="submit" class="btn btn-success">ログイン</button>
             </form>
-        </table>
-    </div>
-</body>
+        </div>
+    </body>
 </html>
